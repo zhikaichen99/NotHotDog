af911524c15ce8a4d9ab372aef16373e23889f41
zhikaichen99 <zhikaichen1999@gmail.com>
2023-01-18 16:20:51 -0500

    added api url

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 
 def predict(filename):
-    API_URL = "https://img.freepik.com/premium-vector/hotdog-pattern-background-food-vector-illustration_463676-5.jpg?w=1380"
+    API_URL = "https://hc6spzm2vl.execute-api.us-east-2.amazonaws.com/production/predict-hotdog"
     
     payload = filename
     headers = {
@@ -27,13 +27,20 @@ if __name__ == "__main__":
     
 
     file = st.file_uploader("Upload an image")
-
+    check_mark = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngaaa.com%2Fdetail%2F888993&psig=AOvVaw1PzrQ-OsrZnmNkfmyzN03k&ust=1674162927219000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDC54aF0vwCFQAAAAAdAAAAABAE'
+    cross_mark = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.emojipng.com%2Fpreview%2F9408103&psig=AOvVaw3gFkaCndRCNhJuNDrApn5w&ust=1674163024491000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiplLWF0vwCFQAAAAAdAAAAABAE'
+    
     if file is not None:
         col1, col2 = st.columns(2)
         col1.image(file, use_column_width = None)
-        col2.metric("Hot Dog 🌭", 2, 4)
-        col2.metric("Not Hot Dog 🥸", 2, 4)
-        col2.write("I don't know what that is 🥸. It's not a hot dog")
-        st.header(predict(file))
+        prediction = predict(file)
+        if prediction == "Hotdog":
+            image = Image.open(check_mark)
+            col2.image(image, use_column_width = None)
+            col2.write(prediction)
+        else:
+            image = Image.open(cross_mark)
+            col2.image(image, use_column_width = None)
+            col2.write(prediction)
 
 
