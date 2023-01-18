42541bd24da0080aaed4c21c3284cd8b10ff44a0
zhikaichen99 <zhikaichen1999@gmail.com>
2023-01-18 16:29:39 -0500

    added emoji pngs

--- app.py
+++ app.py
@@ -27,19 +27,17 @@ if __name__ == "__main__":
     
 
     file = st.file_uploader("Upload an image")
-    check_mark = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngaaa.com%2Fdetail%2F888993&psig=AOvVaw1PzrQ-OsrZnmNkfmyzN03k&ust=1674162927219000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDC54aF0vwCFQAAAAAdAAAAABAE'
-    cross_mark = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.emojipng.com%2Fpreview%2F9408103&psig=AOvVaw3gFkaCndRCNhJuNDrApn5w&ust=1674163024491000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKiplLWF0vwCFQAAAAAdAAAAABAE'
-    
+   
     if file is not None:
         col1, col2 = st.columns(2)
         col1.image(file, use_column_width = None)
         prediction = predict(file)
         if prediction == "Hotdog":
-            image = Image.open(check_mark)
+            image = Image.open('./images/check_emoji.png')
             col2.image(image, use_column_width = None)
             col2.write(prediction)
         else:
-            image = Image.open(cross_mark)
+            image = Image.open('./images/x_emoji.png')
             col2.image(image, use_column_width = None)
             col2.write(prediction)
 
